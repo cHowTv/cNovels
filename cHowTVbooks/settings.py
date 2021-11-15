7d98f63c940e68d88466e75898a704d5a221e9ce
proflamyt <53262578+proflamyt@users.noreply.github.com>
2021-11-15 19:35:06 +0100

    jwt

--- cHowTVbooks/settings.py
+++ cHowTVbooks/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-         'rest_framework.authentication.BasicAuthentication',
+       #  'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         
         'rest_framework.authentication.SessionAuthentication',
