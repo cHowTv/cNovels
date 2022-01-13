96dd63dbd32f544dbc48f9d478eb84ef9d556041
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-01-13 23:44:45 +0100

    added profile

--- authentication/views.py
+++ authentication/views.py
@@ -192,6 +192,8 @@ class ProfileViewset(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+   
+
 
     
     
