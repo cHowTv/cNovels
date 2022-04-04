638309a71e86304357e96db346f64ebb7441f435
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-04-04 10:52:49 +0100

    remove password2 validation

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -60,7 +60,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta :
         model = User 
-        fields = ('username','email', 'password', 'password2')
+        fields = ('username','email', 'password')
 
 
 
