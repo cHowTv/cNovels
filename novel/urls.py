edc98a8fdf2fb8a5adf5be85ed34688ba001a343
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-04-12 09:50:57 +0100

    change url

--- novel/urls.py
+++ novel/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
          schema_view.with_ui('redoc', cache_timeout=0),
          name='schema-redoc'
          ),
-    path('swagger/',
+    path('',
          schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'
          ),
