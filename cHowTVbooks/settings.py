bb24c8339bb03c8164d632a52f76e55f2d676de8
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-04-15 03:31:10 +0100

    ola

--- cHowTVbooks/settings.py
+++ cHowTVbooks/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware' 
 ]
 
 ROOT_URLCONF = 'cHowTVbooks.urls'
